36046de2700a2a7c0b665f3b59365b7bd7c11336
pratama6624 <scyoung6624@gmail.com>
2024-11-09 20:13:25 +0700

    Initial Commit

---
+++ VisionOSMusic/VisionOSMusicApp.swift
@@ -0,0 +1,17 @@
+//
+//  VisionOSMusicApp.swift
+//  VisionOSMusic
+//
+//  Created by Pratama One on 09/11/24.
+//
+
+import SwiftUI
+
+@main
+struct VisionOSMusicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
